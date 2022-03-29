53f2280f5a58afe274939d65c60fe758d5ae13b0
Ehtisham Ayaan <56442970+Ehtisham-Ayaan@users.noreply.github.com>
2022-03-29 22:32:22 +0500

    project started islamic series

--- src/About.js
+++ src/About.js
@@ -1,117 +1,120 @@
-import { Typography } from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import about from './img/about.jpg'
-import {makeStyles} from "@material-ui/core/styles";
-import about111 from './img/about111.jpeg'
-import about222 from './img/about222.jpg'
-import { useState, useEffect } from 'react';
-
+import { Typography } from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { useState, useEffect } from "react";
+import about from "./img/about1.jpg";
 
 const useStyles = makeStyles((theme) => ({
- 
-    h:{
-        marginTop : '20%', 
-        color: "green"
-    },
+  h: {
+    marginTop: "20%",
+    color: "green",
+  },
 
-    text : {
-           fontSize : '15px',
-           overflow: "visible",
-           fontWeight: 800,
-           fontStyle: "italic",
-           fontFamily: `"Inter-ExtraBoldItalic", "Inter", sans-serif`,
-           color: "#433E69",
-           letterSpacing: 1,
-           lineHeight: 1.5,
-    },
+  text: {
+    fontSize: "15px",
+    overflow: "visible",
+    fontWeight: 800,
+    fontStyle: "italic",
+    fontFamily: `"Inter-ExtraBoldItalic", "Inter", sans-serif`,
+    color: "#433E69",
+    letterSpacing: 1,
+    lineHeight: 1.5,
+  },
 
-    frame : {
-        height: "auto",
-        overflow: "visible",
-        display: 'block',
-        marginTop : '10%',
-        marginBottom: '10%',
-      },
+  frame: {
+    height: "auto",
+    overflow: "visible",
+    display: "block",
+    marginTop: "10%",
+    marginBottom: "10%",
+  },
 
-      frame2 : {
-        width: "85%",
-        height: 'auto',
-        background: "radial-gradient(100% 50% at 0% 46.6%, #33aaff 0%, hsl(0, 0%, 66%) 100%)",
-        overflow: "visible",
-      }
-
+  frame2: {
+    width: "85%",
+    height: "auto",
+    background:
+      "radial-gradient(100% 50% at 0% 46.6%, #33aaff 0%, hsl(0, 0%, 66%) 100%)",
+    overflow: "visible",
+  },
 }));
 
 function getWindowDimensions() {
-    const width = window.innerWidth;
-    return width
-    
-  }
+  const width = window.innerWidth;
+  return width;
+}
 
 export default function About() {
-
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-    const [fontSize, setFontSize] = useState(25);
-    const [widthh , setWidthh] = useState(750);
-useEffect(() => {
-
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  const [fontSize, setFontSize] = useState(25);
+  const [setWidthh] = useState(750);
+  useEffect(() => {
     setWindowDimensions(getWindowDimensions());
 
-    if(windowDimensions < 1024){ 
-        setFontSize(12);
-        setWidthh(280);
+    if (windowDimensions < 1024) {
+      setFontSize(12);
+      setWidthh(280);
     }
-})
-    
-    const myClass = useStyles();
+  });
 
-    return(
+  const myClass = useStyles();
 
-        <div className="container">
-            <div className="row">
-                <div className="col-xs-12">
-                    <img src={about} width = '100%' height = 'auto' style = {{marginTop: '10%'}}/>
-                </div>
-            </div>
-            <div className="row">
-            <div className="col-xs-12" style = {{padding : 15}}>
-                    <h4 className={myClass.h}>Who We Are</h4>
-                    <Typography className={myClass.text}>We are new in Software Industry and Build this app for the Users who feel problem while watch their favourite Series. As I noticed that some of the episodes are blocked or have bad Quality, We having Implemented our Best Scrappers which which will fetch out best quality servers for you in no time. If some Video wishes you to wait for playing it is therefore because our bots try their best to provide you Best Quality.</Typography>
-                </div>
-            </div>
-
-            <div className="row">
-            <div className="col-xs-12">
-                    <div className={myClass.frame}>
-                        <div style = {{marginLeft:'10%'}}>    
-                        <img src={about111} width="35%" height = "auto"/>
-                        <img src={about222} width = "50%" height = "auto"/>
-
-                            <div className = {myClass.frame2}>
-            <Typography className = {myClass.text} style = {{fontSize : fontSize , marginLeft : '12%'}}>
-            Designed By: Rabbani Solutions <br/>
-            Feel Free to Contact Us <br/>
-            erwattoo@gmail.com for  <br/>
-            any suggestions or Query <br/> </Typography>
-
-                            </div>
-                            
-
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div className="row">
-            <div className="col-xs-12" style = {{padding : 15}}>
-                    <h4 className={myClass.h}>Where to Find us</h4>
-                    <Typography className={myClass.text}>We are remote For Now... Only COntact Us By Email</Typography>
-                </div>
-            </div>
-            <br/>
-            
-            
-
-
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-12">
+          <img
+            src={about}
+            width="100%"
+            height="auto"
+            style={{ marginTop: "10%" }}
+          />
         </div>
-    )
+      </div>
+      <div className="row">
+        <div className="col-xs-12" style={{ padding: 15 }}>
+          <h4 className={myClass.h}>Who We Are</h4>
+          <Typography className={myClass.text}>
+            We are new in Software Industry and Build this app for the Users who
+            feel problem while watch their favourite Series. As I noticed that
+            some of the episodes are blocked or have bad Quality, We having
+            Implemented our Best Scrappers which which will fetch out best
+            quality servers for you in no time. If some Video wishes you to wait
+            for playing it is therefore because our bots try their best to
+            provide you Best Quality.
+          </Typography>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-xs-12">
+          <div className={myClass.frame}>
+            <div style={{ marginLeft: "10%" }}>
+              <div className={myClass.frame2}>
+                <Typography
+                  className={myClass.text}
+                  style={{ fontSize: fontSize, marginLeft: "12%" }}
+                >
+                  Designed By: Rabbani Solutions <br />
+                  Feel Free to Contact Us <br />
+                  erwattoo@gmail.com for <br />
+                  any suggestions or Query <br />{" "}
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12" style={{ padding: 15 }}>
+          <h4 className={myClass.h}>Where to Find us</h4>
+          <Typography className={myClass.text}>
+            We are remote For Now... Only COntact Us By Email
+          </Typography>
+        </div>
+      </div>
+      <br />
+    </div>
+  );
 }
--- src/home.jsx
+++ src/home.jsx
@@ -1,236 +1,114 @@
-import './css/App.css';
-import {makeStyles} from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import front from './img/front.jpg';
+import "./css/App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import front from "./img/front.jpg";
 
+import React from "react";
 
-import React from 'react';
-
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import logo from "./img/logoA.png";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Typography from "@material-ui/core/Typography";
-
-
-import mySketch from "./img/krulus-osman.jpg"
-import ertugral from "./img/ertugral.png"
-import uyanis from "./img/Uyanis.jpg"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const useStyles = makeStyles((theme) => ({
-    p: {
-            overflow: "visible",
-            letterSpacing: 0.5,
-            color: "#433E69",
-            fontSize: '20px',
-            fontWeight: 700,
-            fontStyle: "italic",
-            fontFamily: `"Inter-ExtraBoldItalic", "Inter", sans-serif`,
-            lineHeight: 2,
-            textAlign: "center",
-            top: '20%' ,
-            height: 'auto',
-            width : '100%'
-            
-        },
-    frame : {
-        width: '100%',
-        height: 'auto',
-    },
+  p: {
+    overflow: "visible",
+    letterSpacing: 0.5,
+    color: "#433E69",
+    fontSize: "20px",
+    fontWeight: 700,
+    fontStyle: "italic",
+    fontFamily: `"Inter-ExtraBoldItalic", "Inter", sans-serif`,
+    lineHeight: 2,
+    textAlign: "center",
+    top: "20%",
+    height: "auto",
+    width: "100%",
+  },
+  frame: {
+    width: "100%",
+    height: "auto",
+  },
 
-    textframe:{
-    width: '100%',
-  height: 'auto',
-  overflow: "hidden",
-//   transform: "translate('-50%' , -'50%' )" ,
-  background: "radial-gradient(92.10000000000001% 50% at 5.2% 52.7%, #2266ff 0%, hsl(222, 57%, 77%) 100%)",
-borderRadius: "5%",   
+  textframe: {
+    width: "100%",
+    height: "auto",
+    overflow: "hidden",
+    //   transform: "translate('-50%' , -'50%' )" ,
+    background:
+      "radial-gradient(92.10000000000001% 50% at 5.2% 52.7%, #2266ff 0%, hsl(222, 57%, 77%) 100%)",
+    borderRadius: "5%",
+  },
 
-},
+  frame1: {
+    width: "100%",
+    height: "auto",
+    overflow: "visible",
+    background:
+      "radial-gradient(100% 50% at 0% 50%, #593b3b 0%, hsla(0, 71%, 76%, 0.66) 100%)",
+  },
 
-frame1 : {
-
-    width: '100%',
-    height: 'auto',
-    overflow: 'visible',
-    background: "radial-gradient(100% 50% at 0% 50%, #593b3b 0%, hsla(0, 71%, 76%, 0.66) 100%)",
-
-},
-
-liText : {
-
-    width: '100%',
-    height: 'auto',
-    overflow: 'visible',
-    color: 'white',
-    textAlign: 'left',
-    lineHeight: '2.3',
-    fontWeight: '700',
-    fontStyle: 'italic',
+  liText: {
+    width: "100%",
+    height: "auto",
+    overflow: "visible",
+    color: "white",
+    textAlign: "left",
+    lineHeight: "2.3",
+    fontWeight: "700",
+    fontStyle: "italic",
     fontFamily: "Inter-Bold" || "Inter" || "sans-serif",
-    fontSize: '14.3px',
-    letterSpacing: '0px',
-},
+    fontSize: "14.3px",
+    letterSpacing: "0px",
+  },
 
-    myStyle : {
-        position: 'absolute',
-        top: '20%' ,
-        left: '35%' ,
-        transform: "translate('-50%' , -'50%' )" ,
-        width: '30%',
-        height: 'auto',
-    },
-    media: {
-      height: 200,
-    },
-    paper: {
-      marginTop: theme.spacing(8),
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: "100%", // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
+  myStyle: {
+    position: "absolute",
+    top: "20%",
+    left: "35%",
+    transform: "translate('-50%' , -'50%' )",
+    width: "30%",
+    height: "auto",
+  },
+  media: {
+    height: 200,
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 function Home() {
+  const width = "100%";
+  const height = "auto";
 
-  const classes = useStyles();
+  const myClass = useStyles();
 
-    const [password, setPassword] = React.useState("");
+  return (
+    <div style={{ overflow: "hidden" }}>
+      <img
+        width={width}
+        height={height}
+        src={front}
+        alt="image"
+        style={{ opacity: 0.8, borderBottomRightRadius: "20%" }}
+      />
 
-    const [myData, setMyData] = React.useState([]);
-
-
-
-    const width = '100%';
-    const height = 'auto';
-
-    const myClass = useStyles();
-
-    return (
-<div style={{overflow : 'hidden'}}>
-<img width = {width} height = {height}
-                     src = {front}
-                     alt="image" 
-                     style = {{  opacity:0.8 , borderBottomRightRadius : '20%' }}
-                     
-                     />
-                     
-
-                     <img src = {logo} className={myClass.myStyle}/>
-
-<div style={{paddingLeft : "10px" , paddingRight : "10px" }}>
-<div className="container">
-<div className="row">
-<div className="col-md-4">
-
-<Link to="/osman" style={{textDecoration: 'none' }} >
-                            
-<Card className={classes.root} style = {{marginTop : "14%" , marginBottom : "10%"}} >
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image = {mySketch}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" style={{textTransform: "uppercase"}}>
-          Krulus Osman
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           Click To watch Series
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      {/* <CardActions>
-      
-      </CardActions> */}
-    </Card>
-    </Link>
-
+      <img src={logo} className={myClass.myStyle} />
     </div>
-    
-    <div className="col-md-4">
-
-<Link to="/ertugral" style={{textDecoration: 'none' }} >
-                            
-<Card className={classes.root} style = {{marginTop : "14%" , marginBottom : "10%"}} >
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image = {ertugral}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" style={{textTransform: "uppercase"}}>
-          Drillis Ertugral
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           Click To watch Series
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      {/* <CardActions>
-      
-      </CardActions> */}
-    </Card>
-    </Link>
-
-    </div>
-    
-    <div className="col-md-4">
-
-<Link to="/uyanis" style={{textDecoration: 'none' }} >
-                            
-<Card className={classes.root} style = {{marginTop : "14%" , marginBottom : "10%"}} >
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image = {uyanis}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" style={{textTransform: "uppercase"}}>
-          Uyanis: Büyük Selcuklu
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           Click To watch Series
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      {/* <CardActions>
-      
-      </CardActions> */}
-    </Card>
-    </Link>
-
-    </div></div>
-<div className="row">
-
-  <h3 className="blink_me" style={{marginTop:"5%" , marginBottom: "5%"}}> Be connected for BARBAROSA </h3>
-
-    </div>
-</div>
-
-</div>
-</div>
-
-
-
-
-    )
+  );
 }
 export default Home;
